27741771965d981a2354f034edfa63ed98fb5535
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-03-03 17:52:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.cpp
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.cpp
@@ -43,17 +43,16 @@ class Solution {
   public:
     // Function to check if the linked list has a loop.
     bool detectLoop(Node* head) {
-        Node* slow=head;
-        Node* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-        if(slow==fast){
-            return true;
-        }
-        }
-        return false;
-        // your code here
+       Node* slow=head;
+       Node* fast=head;
+       while(fast!=NULL && fast->next!=NULL){
+           slow=slow->next;
+           fast=fast->next->next;
+           if(slow==fast){
+               return true;
+           }
+       }
+       return false;
     }
 };
 
