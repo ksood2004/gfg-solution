eba51a5c7c5115e1651de586a9f289b555bded4d
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-03-03 18:42:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -8,19 +8,24 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function Template for C++
 
 class Solution {
   public:
     int largest(vector<int> &arr) {
-        int maxx=0;
         int n=arr.size();
+        int maxx=arr[0];
         for(int i=0;i<n;i++){
-            maxx=max(maxx,arr[i]);
+            if(arr[i]>maxx){
+                maxx=arr[i];
+            }
+            
         }
         return maxx;
     }
 };
+
 
 
 //{ Driver Code Starts.
