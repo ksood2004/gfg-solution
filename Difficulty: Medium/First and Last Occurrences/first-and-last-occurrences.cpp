55f30f312bcafaaa6959e7c5425cba4a3955e7ed
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-23 19:18:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
@@ -1,89 +1,55 @@
-//{ Driver Code Starts
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-  int first(vector<int>& arr, int x,int n,int start,int end){
-        int mid;
-        int karan=-1;
-        if(start>=end){
-            return -1;
-        }
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(arr[mid]==x){
-                karan=mid;
-                end=mid-1;
-            }
-            else if(arr[mid]>x){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-        mid=start+(end-start)/2;   
-        }
-        return karan;
+  int first(vector<int>& arr, int x){
+      int n=arr.size();
+      int s=0;
+      int e=n-1;
+      int ans=-1;
+      while(s<=e){
+          int mid=s+(e-s)/2;
+          if(arr[mid]==x){
+              ans=mid;
+              e=mid-1;
+          }
+          else if(arr[mid]<x){
+              
+              s=mid+1;
+          }
+          else{
+              e=mid-1;
+          }
+      }
+      return ans;
+      
   }
-  int second(vector<int>& arr, int x,int n,int start,int end){
-        int mid;
-        int karan=-1;
-        if(start>=end){
-            return -1;
-        }
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(arr[mid]==x){
-                karan=mid;
-                start=mid+1;
-            }
-            else if(arr[mid]>x){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-        mid=start+(end-start)/2;   
-        }
-        return karan;
+  int second(vector<int>& arr, int x){
+      int n=arr.size();
+      int s=0;
+      int e=n-1;
+      int ans=-1;
+      while(s<=e){
+          int mid=s+(e-s)/2;
+          if(arr[mid]==x){
+              ans=mid;
+              s=mid+1;
+          }
+          else if(arr[mid]<x){
+              
+              s=mid+1;
+          }
+          else{
+              
+              e=mid-1;
+          }
+      }
+      return ans;
+      
   }
-vector<int> find(vector<int>& arr, int x) {
-    int c=first(arr,x,arr.size(),0,arr.size()-1);
-    int d=second(arr,x,arr.size(),0,arr.size()-1);
-    return {c,d};
-}    
-};
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        int x;
-        cin >> x;
-        cin.ignore();
-        vector<int> ans;
-        Solution ob;
-        ans = ob.find(arr, x);
-        cout << ans[0] << " " << ans[1] << endl;
-        cout << "~" << endl;
+    vector<int> find(vector<int>& arr, int x) {
+        vector<int> karan(2);
+        karan[0]=first(arr,x);
+        karan[1]=second(arr,x);
+        return karan;
+        
     }
-    return 0;
-}
-
-// } Driver Code Ends
+};
