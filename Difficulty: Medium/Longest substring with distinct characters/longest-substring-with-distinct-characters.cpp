336d96299faac12097e370328f9c8ed0c59721c8
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-23 17:43:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.cpp
+++ Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.cpp
@@ -1,60 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int longestUniqueSubstr(string &s) {
-        int i=0;
-        int j=0;
-        int n=s.size();
-        map<char,int> karan;
-        int cnt=0;
-        int maxx=0;
-        while(j<n){
+        int n = s.size();
+        unordered_map<char, int> karan;
+        int i = 0, j = 0;
+        int maxx = 0;
+
+        while (j < n) {
             karan[s[j]]++;
-            
-        
-                while(karan[s[j]]>1){
-                    karan[s[i]]--;
-                    
-                    if(karan[s[i]]==0){
-                        karan.erase(karan[s[i]]);
-                    }
-                    i++;
-                }
-            
-            maxx=max(maxx,j-i+1);
+
+            // If duplicate character found, shrink window from the left
+            while (karan[s[j]] > 1) {
+                karan[s[i]]--;
+                i++;
+            }
+
+            // Update max length of unique substring
+            maxx = max(maxx, j - i + 1);
             j++;
-            
         }
+
         return maxx;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
-
-        Solution obj;
-        string s;
-        cin >> s;
-        cout << obj.longestUniqueSubstr(s) << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
