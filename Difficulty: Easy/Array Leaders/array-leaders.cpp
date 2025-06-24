7a31f5350231dd09af7c3922896e99abb739ca4f
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-24 20:22:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -4,19 +4,19 @@ class Solution {
     // Function to find the leaders in the array.
   public:
     vector<int> leaders(vector<int>& arr) {
-        vector<int> karan;
+        // Code here
         int n=arr.size();
         int maxx=arr[n-1];
+        
+        vector<int> karan;
         karan.push_back(maxx);
         for(int i=n-2;i>=0;i--){
             if(arr[i]>=maxx){
                 maxx=arr[i];
                 karan.push_back(maxx);
-                
             }
         }
-        reverse(karan.begin(),karan.end());
-        return karan;
-        
+       reverse(karan.begin(),karan.end());
+        return  karan;
     }
 };
