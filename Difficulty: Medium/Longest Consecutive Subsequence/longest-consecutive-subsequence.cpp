6d55481e732ce44270a9dd17fd65b39490cdc9c7
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-24 10:01:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
@@ -1,60 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
 
     // Function to return length of longest subsequence of consecutive integers.
     int longestConsecutive(vector<int>& arr) {
-        int length=1;
-        int karan=1;
+        // Your code here
         int n=arr.size();
         sort(arr.begin(),arr.end());
+        int maxx=0;
+        int cnt=1;
         for(int i=1;i<n;i++){
             if(abs(arr[i]-arr[i-1])==1){
-                length++;
+                cnt++;
             }
-            else if(arr[i]-arr[i-1]==0){
+            else if(abs(arr[i]-arr[i-1])==0){
                 
             }
-            else{
-                length=1;
+            else {
+                cnt=1;
             }
-            karan=max(karan,length);
+            maxx=max(maxx,cnt);
         }
-        return karan;
+        return maxx;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.longestConsecutive(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
