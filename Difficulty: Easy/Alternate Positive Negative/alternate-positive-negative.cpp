924cd5c9c24d62a3b9f63a02669bf8479c00098e
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-24 20:10:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.cpp
+++ Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.cpp
@@ -2,38 +2,37 @@
 class Solution {
   public:
     void rearrange(vector<int> &arr) {
-        vector<int> ans, pos, neg;
-        int n = arr.size();
-
-        // Separate positives and negatives
-        for (int i = 0; i < n; i++) {
-            if (arr[i] >= 0) {
+        // code here
+        int n=arr.size();
+        vector<int> ans,pos,neg;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=0){
                 pos.push_back(arr[i]);
-            } else {
+            }
+            else{
                 neg.push_back(arr[i]);
             }
         }
-
-        int i = 0, j = 0;
-
-        // Add alternately
-        while (i < pos.size() && j < neg.size()) {
+        
+        int i=0;
+        int j=0;
+        while(i<pos.size() && j<neg.size()){
             ans.push_back(pos[i++]);
+           
             ans.push_back(neg[j++]);
+            
         }
-
-        // Add remaining positives
-        while (i < pos.size()) {
+        
+        while(i<pos.size()){
             ans.push_back(pos[i++]);
+            
         }
-
-        // Add remaining negatives
-        while (j < neg.size()) {
+        while(j<neg.size()){
             ans.push_back(neg[j++]);
+          
         }
         for(int k=0;k<n;k++){
             arr[k]=ans[k];
         }
-        
     }
 };
