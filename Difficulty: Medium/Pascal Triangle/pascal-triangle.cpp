a66417bf9c7988e6645cc53acd35a8cef032a970
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-25 20:11:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pascal Triangle/pascal-triangle.cpp
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.cpp
@@ -1,48 +1,16 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 
 class Solution {
   public:
     vector<int> nthRowOfPascalTriangle(int n) {
-         vector<int>ans;
-            int coef=1;
-            for(int j=1;j<=n;j++)
-            {
-                ans.push_back(coef);
-                coef=coef*(n-j)/j;
-            }
-        return ans;
-    
+        // code here
+        long long int ans=1;
+        vector<int> karan;
+        karan.push_back(1);
+        for(int i=1;i<n;i++){
+            ans=ans*(n-i);
+            ans=ans/i;
+            karan.push_back(ans);
+        }
+        return karan;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-void printAns(vector<int> &ans) {
-    for (auto &x : ans) {
-        cout << x << " ";
-    }
-    cout << "\n";
-}
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        Solution ob;
-        auto ans = ob.nthRowOfPascalTriangle(n);
-        printAns(ans);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
