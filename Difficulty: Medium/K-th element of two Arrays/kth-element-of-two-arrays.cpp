0f6e32a3c9d2513936a3a9cd6e0f96c058d3cabc
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-29 20:13:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,63 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int kthElement(vector<int>& a, vector<int>& b, int k) {
-         vector<int> merged;
-
-        // Merging both arrays
-        merged.insert(merged.end(), a.begin(), a.end());
-        merged.insert(merged.end(), b.begin(), b.end());
-
-        // Sorting the merged array
-        sort(merged.begin(), merged.end());
-
-        // Check if K is valid
-        if (k > 0 && k <= merged.size()) {
-            return merged[k - 1]; // K-th element is at index k - 1
-        } else {
-            return -1; // Return -1 if K is out of range
+        // code here
+        vector<int> karan;
+        karan.insert(karan.end(),a.begin(),a.end());
+        karan.insert(karan.end(),b.begin(),b.end());
+        sort(karan.begin(),karan.end());
+        priority_queue<int> pq;
+        int n=karan.size();
+        for(int i=0;i<n;i++){
+            pq.push(karan[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
+            
         }
+        return pq.top();
     }
-    
 };
-
-
-//{ Driver Code Starts.
-
-// Driver code
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, m, k;
-        cin >> k;
-        cin.ignore();
-        string input;
-        int num;
-        vector<int> a, b;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            a.push_back(num);
-        }
-
-        getline(cin, input);
-        stringstream s3(input);
-        while (s3 >> num) {
-            b.push_back(num);
-        }
-
-        Solution ob;
-        cout << ob.kthElement(a, b, k) << endl << "~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
