88cb0137d28b3ade067f34943a6f79611b80cddd
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-29 15:35:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.cpp
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -1,26 +1,19 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    // Function to find the next greater element for each element of the array.
     vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
         int n=arr.size();
-        stack<int> s;
         vector<int> karan;
+        stack<int> s;
         for(int i=n-1;i>=0;i--){
-            if(s.empty()){
+            if(s.size()==0){
                 karan.push_back(-1);
             }
-            else if(s.size()>0 && s.top()>arr[i]){
+            else if(s.size()>0 && arr[i]<s.top()){
                 karan.push_back(s.top());
             }
-            else if(s.size()>0 && s.top()<=arr[i]){
-                while(s.size()>0 && s.top()<=arr[i]){
+            else if(s.size()>0 && arr[i]>=s.top()){
+                while(s.size()>0 && arr[i]>=s.top()){
                     s.pop();
                 }
                 if(s.size()==0){
@@ -36,39 +29,3 @@ class Solution {
         return karan;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t; // Number of test cases
-    cin >> t;
-    cin.ignore(); // Ignore the newline after reading t
-    while (t--) {
-        vector<int> a;
-        string input;
-
-        // Reading the entire input line for the array
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num); // Read the array elements from input string
-
-        Solution obj;
-        vector<int> result = obj.nextLargerElement(a);
-
-        // Print the result in the required format
-        for (int i = 0; i < result.size(); i++) {
-            if (i != 0)
-                cout << " ";
-            cout << result[i];
-        }
-        cout << endl;        // Ensure new line after each test case output
-        cout << "~" << endl; // Ensure new line after each test case output
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
