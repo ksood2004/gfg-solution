41a0f291c31d2ba9694e55ac42b95c73845621cb
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-01 18:34:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.cpp
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 class Solution {
   public:
@@ -69,30 +61,3 @@ class Solution {
 };
 
 
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        int res = obj.getMaxArea(arr);
-        cout << res << endl;
-        cout << "~" << endl;
-        // string tl;
-        // getline(cin, tl);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
