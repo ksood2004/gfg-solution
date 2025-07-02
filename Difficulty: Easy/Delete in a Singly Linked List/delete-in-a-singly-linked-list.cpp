d981a951d123254d1a29785a22acae5fcf27c0fd
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-02 20:53:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete in a Singly Linked List/delete-in-a-singly-linked-list.cpp
+++ Difficulty: Easy/Delete in a Singly Linked List/delete-in-a-singly-linked-list.cpp
@@ -1,31 +1,3 @@
-//{ Driver Code Starts
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-using namespace std;
-
-/* Link list Node */
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void print(Node* root) {
-    Node* temp = root;
-    while (temp != NULL) {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-}
-
-
-// } Driver Code Ends
 /* Link list Node
 struct Node
 {
@@ -40,10 +12,14 @@ struct Node
 */
 class Solution {
   public:
-    Node* deletee(Node* head,int x){
-                if(head==NULL){
-            return head;
+    /* Function to delete a node from a linked list */
+    Node* deleteNode(Node* head, int x) {
+        // code here
+        if(head==NULL){
+            return NULL;
         }
+        int cnt=1;
+        
         if(x==1){
             Node* temp=head;
             head=head->next;
@@ -51,81 +27,19 @@ class Solution {
             return head;
         }
         Node* temp=head;
-        Node* prev=NULL;
-        int cnt=1;
-        
-        
-        while (temp != NULL) {
-            if (cnt == x) {
-                prev->next = temp->next;
+        Node* prev=head;
+        while(temp!=NULL){
+            if(cnt==x){
+                prev->next=prev->next->next;
                 delete temp;
-                break;
+                //prev=temp;
             }
-            prev = temp;
-            temp = temp->next;
             cnt++;
+            prev=temp;
+            temp=temp->next;
+            
         }
-        
         return head;
+        
     }
-
-    Node* deleteNode(Node* head, int x) {
-        return deletee(head,x);
-}
 };
-
-//{ Driver Code Starts.
-
-/* Driver program to test above function */
-int main() {
-    int T;
-    cin >> T;
-    cin.ignore(); // Clear newline character after T input
-
-    while (T--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        if (arr.empty()) {
-            cout << -1 << endl;
-            continue;
-        }
-
-        int data = arr[0];
-        Node* head = new Node(data);
-        Node* tail = head;
-
-        for (int i = 1; i < arr.size(); ++i) {
-            data = arr[i];
-            tail->next = new Node(data);
-            tail = tail->next;
-        }
-
-        int k;
-        cin >> k;
-        cin.ignore(); // Clear newline character after k input
-
-        Solution ob;
-        Node* result = ob.deleteNode(head, k);
-        print(result);
-        cout << endl;
-
-        // Clean up memory to prevent memory leaks
-        while (result != NULL) {
-            Node* temp = result;
-            result = result->next;
-            delete temp;
-        }
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
