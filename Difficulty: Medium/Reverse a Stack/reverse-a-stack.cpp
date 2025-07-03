5dd81e77b5b13e9023ce2f8228d82003bf00d763
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-03 19:07:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
@@ -1,69 +1,26 @@
-//{ Driver Code Starts
-//Initial Template for C++
+// User function Template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-void insert(stack<int>& s, int val){
-    if(s.size()==0){
-        s.push(val);
-        return;
-    }
-    int temp=s.top();
-    s.pop();
-    insert(s,val);
-    s.push(temp);
-    return ;
-}
-    void Reverse(stack<int> &St){
-        if(St.size()==0){
-            return;
-        }
-        int temp=St.top();
-        St.pop();
-        Reverse(St);
-        insert(St,temp);
-        
+class Solution {
+  public:
+  
+  void rev(stack<int>& s,int k){
+      if(s.size()==0){
+          s.push(k);
+          return ;
+      }
+      int val=s.top();
+      s.pop();
+      rev(s,k);
+      s.push(val);
+  }
+    void Reverse(stack<int> &St) {
+        int n=St.size();
+       if(n==0 || n==1){
+           return ;
+       }
+       int temp=St.top();
+       St.pop();
+       Reverse(St);
+       rev(St,temp);
     }
 };
-
-//{ Driver Code Starts.
-
-
-int main(){
-    int T;
-    cin>>T;
-    while(T--){
-        int N;
-        cin>>N;
-        stack<int> St;
-        for(int i=0;i<N;i++){
-            int x;
-            cin>>x;
-            St.push(x);
-        }
-        Solution ob;
-        ob.Reverse(St);
-        vector<int>res;
-        while(St.size())
-        {
-            res.push_back(St.top());
-            St.pop();
-        }
-        for(int i = res.size()-1;i>=0;i--)
-        {
-            cout<<res[i]<<" ";
-        }
-        
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-}
-// } Driver Code Ends
