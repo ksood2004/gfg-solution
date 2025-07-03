3d4f88cf98efd18b0b5f0087c36ddeeaaa2e5e10
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-03 18:48:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -1,45 +1,27 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-class SortedStack{
-public:
-	stack<int> s;
-	void sort();
-};
-
-void printStack(stack<int> s)
-{
-    while (!s.empty())
-    {
-        printf("%d ", s.top());
-       	s.pop();
+void insert(stack<int>&s , int temp){
+    if(s.size()==0 || s.top()<=temp){
+        s.push(temp);
+        return ;
     }
-    printf("\n");
+    
+    int val=s.top();
+    s.pop();
+    insert(s,temp);
+    s.push(val);
+    
+}
+void SortedStack ::sort() {
+   if(s.size()<=1){
+       return ;
+   }
+   int temp=s.top();
+   s.pop();
+   sort();
+   insert(s,temp);
+    
+    
 }
 
-int main()
-{
-int t;
-cin>>t;
-while(t--)
-{
-	SortedStack *ss = new SortedStack();
-	int n;
-	cin>>n;
-	for(int i=0;i<n;i++)
-	{
-	int k;
-	cin>>k;
-	ss->s.push(k);
-	}
-	ss->sort();
-	printStack(ss->s);
-
-cout << "~" << "\n";
-}
-}
-// } Driver Code Ends
 
 
 /*The structure of the class is
@@ -52,27 +34,27 @@ public:
 
 /* The below method sorts the stack s 
 you are required to complete the below method */
-void insert(stack<int> &s,int val){
-    if(s.size()==0 || s.top()<=val){
-        s.push(val);
-        return ;
-    }
-    int temp=s.top();
-    s.pop();
-    insert(s,val);
+// void insert(stack<int> &s,int val){
+//     if(s.size()==0 || s.top()<=val){
+//         s.push(val);
+//         return ;
+//     }
+//     int temp=s.top();
+//     s.pop();
+//     insert(s,val);
     
-    s.push(temp);
-    return;
+//     s.push(temp);
+//     return;
     
-}
-void SortedStack :: sort()
-{
-   if(s.size()<=1){
-       return ;
-   }
-   int val=s.top();
-   s.pop();
-   sort();
-   insert(s,val);
+// }
+// void SortedStack :: sort()
+// {
+//   if(s.size()<=1){
+//       return ;
+//   }
+//   int val=s.top();
+//   s.pop();
+//   sort();
+//   insert(s,val);
    
-}
+// }
