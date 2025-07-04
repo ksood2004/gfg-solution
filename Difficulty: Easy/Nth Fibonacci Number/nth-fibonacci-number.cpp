ca6a05c593267c72feba074ba770906687c1ae4f
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-04 18:19:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+// User function Template for C++
+class Solution {
+  public:
+  int t[31];
+  int karan(int n){
+      t[0]=0;
+      t[1]=1;
+      for(int i=2;i<=n;i++){
+          t[i]=t[i-1]+t[i-2];
+      }
+      return t[n];
+  }
+    int nthFibonacci(int n) {
+        memset(t,0,sizeof(t));
+        return karan(n);
+        
+    }
+};
