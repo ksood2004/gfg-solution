d85623ecdfac63b74acf3b931abbe87d749d578e
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-04 14:58:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subsets/subsets.cpp
@@ -0,0 +1,28 @@
+// User function Template for C++
+
+class Solution {
+  public:
+  void sub( vector<int>& ip,vector<int>& op,vector<vector<int>>& karan){
+    if(ip.size()==0){
+        karan.push_back(op);
+        return ;
+    }
+    vector<int> op1=op;
+    vector<int> op2=op;
+    op1.push_back(ip[0]);
+    vector<int>erase(ip.begin()+1,ip.end());
+    sub(erase,op1,karan);
+    sub(erase,op2,karan);
+
+
+}
+    vector<vector<int>> subsets(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+         vector<vector<int>> karan;
+        vector<int> op;
+        sub(arr,op,karan);
+       sort(karan.begin(),karan.end());
+        return karan;
+        
+    }
+};
