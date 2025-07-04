7007bdd6af2dc855a0397053ddb38d5764a0e62b
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-04 14:45:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -1,41 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    // You need to complete this function
-
+  void hanoi(int n,int from,int to,int aux){
+      if(n==0){
+            return ;
+        }
+        
+        towerOfHanoi(n-1,from,to,aux);
+        towerOfHanoi(n-1,from,aux,to);
+  }
     int towerOfHanoi(int n, int from, int to, int aux) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        
         return pow(2,n)-1;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int T;
-    cin >> T; // testcases
-    while (T--) {
-
-        int N;
-        cin >> N; // taking input N
-
-        // calling toh() function
-        Solution ob;
-
-        cout << ob.towerOfHanoi(N, 1, 3, 2) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
