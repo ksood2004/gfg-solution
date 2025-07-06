31bd34db2256d978bffd1327e6e027f66aefd1bf
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-06 18:52:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
+++ Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -1,17 +1,10 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
 
   public:
-  int t[201][200001];
-  vector<int> karan(vector<int>& arr,int n,int range){
-      int t[n+1][range+1];
+  int t[101][10001];
+  vector<int> karan(vector<int>& arr,int n,int sum){
       for(int i=0;i<=n;i++){
-          for(int j=0;j<=range;j++){
+          for(int j=0;j<=sum;j++){
               if(i==0){
                   t[i][j]=0;
               }
@@ -20,9 +13,8 @@ class Solution {
               }
           }
       }
-      
       for(int i=1;i<=n;i++){
-          for(int j=1;j<=range;j++){
+          for(int j=1;j<=sum;j++){
               if(arr[i-1]<=j){
                   t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
               }
@@ -31,55 +23,28 @@ class Solution {
               }
           }
       }
-      vector<int> karann;
-      for(int j=0;j<=range;j++){
+       vector<int> karann;
+      for(int j=0;j<=sum;j++){
           if(t[n][j]){
               karann.push_back(j);
           }
       }
       return karann;
+     
   }
     int minDifference(vector<int>& arr) {
         int n=arr.size();
         int sum=0;
-        //vector<int> karann;
-        int minn=INT_MAX;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            
         }
+        int mn=INT_MAX;
+        memset(t,0,sizeof(t));
         vector<int> hello=karan(arr,n,sum/2);
-        
         for(int i=0;i<hello.size();i++){
-            int subsetSum = hello[i];
-    minn = min(minn, sum - 2 * subsetSum);
+            int subsetsum=hello[i];
+            mn=min(mn,sum-2*subsetsum);
         }
-        return minn;
+        return mn;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int ans = ob.minDifference(arr);
-
-        cout << ans << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
