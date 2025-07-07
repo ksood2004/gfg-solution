02d121ec0995da906387200afdfc9ad84dab0267
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-07 19:04:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -1,27 +1,32 @@
+// User function Template for C++
+
 class Solution {
   public:
   int t[1001][1001];
-  int karan(int wtt,int n,vector<int>& val, vector<int>& wt){
+  int karan(int n,int m,vector<int>&val,vector<int>& wt){
+      
       for(int i=0;i<=n;i++){
-          for(int j=0;j<=wtt;j++){
+          for(int j=0;j<=m;j++){
               if(i==0 || j==0){
                   t[i][j]=0;
               }
-              else if(wt[i-1]<=j){
+          }
+      }
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+              if(wt[i-1]<=j){
                   t[i][j]=max(val[i-1]+t[i][j-wt[i-1]],t[i-1][j]);
               }
               else{
                   t[i][j]=t[i-1][j];
               }
-              
           }
       }
-      return t[n][wtt];
+      return t[n][m];
   }
     int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
-        int n=val.size();
         memset(t,0,sizeof(t));
-        return karan(capacity,n,val,wt);
+        return karan(val.size(),capacity,val,wt);
         
     }
 };
