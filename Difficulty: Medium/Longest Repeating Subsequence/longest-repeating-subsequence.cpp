341778c6b89db9242f71303b1008100cc0af04ae
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-09 18:31:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.cpp
+++ Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.cpp
@@ -1,14 +1,6 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-  int karan(string& s){
-      string a=s;
-      int n=a.size();
+  int karan(string &s,int n){
       string b=s;
       int m=b.size();
       int t[n+1][m+1];
@@ -16,12 +8,12 @@ class Solution {
           for(int j=0;j<=m;j++){
               if(i==0 || j==0){
                   t[i][j]=0;
-              }
           }
       }
+      }
       for(int i=1;i<=n;i++){
-          for(int j=1;j<=n;j++){
-              if(a[i-1]==b[j-1] && i!=j){
+          for(int j=1;j<=m;j++){
+              if(s[i-1]==b[j-1] && i!=j){
                   t[i][j]=1+t[i-1][j-1];
               }
               else{
@@ -29,29 +21,11 @@ class Solution {
               }
           }
       }
-      
       return t[n][m];
   }
     int LongestRepeatingSubsequence(string &s) {
-        return karan(s);
+        int n=s.size();
+        return karan(s,n);
         
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        string str;
-        cin >> str;
-        Solution obj;
-        int ans = obj.LongestRepeatingSubsequence(str);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
