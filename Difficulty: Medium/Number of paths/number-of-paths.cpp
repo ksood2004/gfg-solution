f6155966596e14138c0e94fc7cab7cd6083c31c0
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-12 14:19:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of paths/number-of-paths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+   int karan(int m, int n) {
+        int t[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    t[i][j] = 0;
+                }
+            }
+        }
+
+        t[1][1] = 1; 
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) continue; 
+                t[i][j] = t[i - 1][j] + t[i][j - 1];
+            }
+        }
+
+        return t[m][n];
+    }
+
+    int numberOfPaths(int m, int n) {
+        // Code Here
+        return karan(m, n);
+    }
+};
