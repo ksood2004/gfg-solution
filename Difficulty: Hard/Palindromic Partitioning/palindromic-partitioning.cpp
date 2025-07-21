72a023f84fd8dd2998b2f80bdcfa7d28176439b4
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-21 21:47:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Palindromic Partitioning/palindromic-partitioning.cpp
+++ Difficulty: Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -1,69 +1,39 @@
-//{ Driver Code Starts
-// Initial Template for c++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 
 class Solution {
   public:
-  
-    int t[1001][1001]; // Memoization table
-
-// Helper function to check if a substring is a palindrome
-bool isPalindrome(string &s, int i, int j) {
-    while (i < j) {
-        if (s[i] != s[j]) return false;
-        i++;
-        j--;
+  int t[1001][1001];
+ bool palindrome(string &s,int i,int j){
+     int n=s.size();
+     while(i<j){
+         if(s[i] != s[j]){
+             return false;
+         }
+         i++;
+         j--;
+     }
+     return true;
+ }
+  int solve(string&s , int i,int j){
+      if(i>j ||  palindrome(s, i, j)){
+          return 0;
+      }
+      if(t[i][j]!=-1){
+          return t[i][j];
+      }
+      int cnt=INT_MAX;
+      for(int k=i;k<j;k++){
+          if(palindrome(s,i,k)){
+              int karan=1+solve(s,k+1,j);
+              cnt=min(cnt,karan);
+          }
+      }
+      return t[i][j]=cnt;
+      
+  }
+    int palPartition(string &s) {
+        memset(t,-1,sizeof(t));
+        return solve(s,0,s.size()-1);
+        
     }
-    return true;
-}
-
-// Recursive function to calculate minimum cuts
-int karan(int i, int j, string &s) {
-    if (i >= j || isPalindrome(s, i, j)) {
-        return 0; // No cuts needed if the substring is already a palindrome
-    }
-
-    if (t[i][j] != -1) {
-        return t[i][j]; // Return the memoized result
-    }
-
-    int hello = INT_MAX;
-
-    for (int k = i; k < j; k++) {
-        // Only partition if the left part is a palindrome
-        if (isPalindrome(s, i, k)) {
-            int temp = 1 + karan(k + 1, j, s); // 1 cut + recursive call for the right part
-            hello = min(hello, temp);
-        }
-    }
-
-    return t[i][j] = hello; // Store and return the result
-}
-
-int palindromicPartition(string &s) {
-    memset(t, -1, sizeof(t)); // Initialize memoization table with -1
-    return karan(0, s.size() - 1, s);
-}
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string str;
-        cin >> str;
-
-        Solution ob;
-        cout << ob.palindromicPartition(str) << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
