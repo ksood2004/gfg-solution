c19bb9e9ffa0741654db25108456fecf9c275221
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-21 21:32:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -1,64 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
- 
 class Solution {
   public:
   int t[1001][1001];
-  int karan(vector<int>& arr,int i,int j){
+  int solve(vector<int>& arr,int i,int j){
       if(i>=j){
           return 0;
       }
-      //int t[i+1][j+1];
-      if (t[i][j] != -1) { 
-            return t[i][j]; // Return precomputed result if available
-        }
-      int minn=INT_MAX;
-      for(int k=i;k<=j-1;k++){
-          int temp=karan(arr,i,k)+karan(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
-          minn=min(minn,temp);
+     
+      
+      if(t[i][j]!=-1){
+          return t[i][j];
       }
-      return t[i][j]=minn;
+      int mn=INT_MAX;
+      
+      for(int k=i;k<=j-1;k++){
+          int temp=solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
+          mn=min(mn,temp);
+      }
+      return t[i][j]=mn;
   }
     int matrixMultiplication(vector<int> &arr) {
-         for(int i=0;i<=arr.size();i++){
-          for(int j=0;j<=arr.size();j++){
-              
-                  t[i][j]=-1;
-              
-          }
-      }
-        return karan(arr,1,arr.size()-1);
+        memset(t,-1,sizeof(t));
+        return solve(arr,1,arr.size()-1);
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.matrixMultiplication(a) << endl << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
