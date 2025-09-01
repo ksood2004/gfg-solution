0d1deddfca2443991d8c9029f575afdb8f63313d
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-09-01 16:52:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        //  code here
+        int togas=0;
+        int tocost=0;
+        int curr=0;
+        int st=0;
+        int n=gas.size();
+        for(int i=0;i<n;i++){
+            togas+=gas[i];
+            tocost+=cost[i];
+
+            curr+=gas[i]-cost[i];
+            if(curr<0){
+                st=i+1;
+                curr=0;
+            }
+        }
+        if(togas<tocost){
+            return -1;
+        }
+        return st;
+    }
+};
